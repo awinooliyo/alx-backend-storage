1323059508b479d4f14e191e54ac6b0c475bd618
awinooliyo <erick.otieno89@gmail.com>
2024-07-17 21:50:03 +0300

    redis advanced corrected

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
-""" expiring web cache module """
+"""Expiring web cache module"""
 
 import redis
 import requests
 from typing import Callable
 from functools import wraps
 
-redis = redis.Redis()
+# Initialize Redis client
+redis_client = redis.Redis()
 
 
 def wrap_requests(fn: Callable) -> Callable:
-    """ Decorator wrapper """
+    """Decorator to wrap requests and cache results"""
 
     @wraps(fn)
-    def wrapper(url):
-        """ Wrapper for decorator guy """
-        redis.incr(f"count:{url}")
-        cached_response = redis.get(f"cached:{url}")
+    def wrapper(url: str) -> str:
+        """Wrapper function to count and cache requests"""
+        redis_client.incr(f"count:{url}")
+        cached_response = redis_client.get(f"cached:{url}")
         if cached_response:
             return cached_response.decode('utf-8')
         result = fn(url)
-        redis.setex(f"cached:{url}", 10, result)
+        redis_client.setex(f"cached:{url}", 10, result)
         return result
 
     return wrapper
@@ -28,7 +29,13 @@ def wrap_requests(fn: Callable) -> Callable:
 
 @wrap_requests
 def get_page(url: str) -> str:
-    """get page self descriptive
-    """
+    """Get the HTML content of a URL and cache it"""
     response = requests.get(url)
     return response.text
+
+
+if __name__ == "__main__":
+    url = ("http://slowwly.robertomurray.co.uk/delay/5000/url/"
+           "http://www.google.com")
+    print(get_page(url))
+    print(get_page(url))
